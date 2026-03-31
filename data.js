53c9fead076039a1ca9e75027f5f62874684e6de
kimsongyi85 <kimsongyi85@gmail.com>
2026-03-31 19:13:55 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+const PROJECT_DATA = {
+    "roast": {
+        title: "ROAST ARCHIVE",
+        category: "Coffee Roastery",
+        concept: "커피의 본질을 아카이브하는 미니멀 브랜딩...",
+        colors: ["#1a1a1a", "#d4a373", "#fefae0"],
+        images: ["img1.jpg", "img2.jpg", "img3.jpg"]
+    },
+    "nuvola": {
+        title: "NuvoLA Perfume",
+        category: "Fragrance",
+        concept: "구름을 닮은 몽환적인 향수 브랜드...",
+        colors: ["#adc1e3", "#ffffff", "#333333"],
+        images: ["p1.jpg", "p2.jpg"]
+    }
+    // ... 이런 식으로 15개를 쭉 적으면 끝!
+};
